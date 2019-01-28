36d8c5299c02e9552bc8703c809953f8dbc267f3
Rogerio <rogerio_pavan@hotmail.com>
2019-01-28 02:53:52 -0200

    trocado mysql conexao

--- config/mysql_connection.js
+++ config/mysql_connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 let mysql_connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'd$fdikUUidoOPJ0onHjl',
-    database: 'igreja_demo'
+    password: '$fdikUUidoOPJ0onHjl',
+    database: 'data'
 });
 
 module.exports = mysql_connection;

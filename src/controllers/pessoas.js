fb0c067a616a0370bd0240d9dfec76b3a3bef3ac
Rogerio <rogerio_pavan@hotmail.com>
2019-02-02 18:05:50 -0200

    commit

--- src/controllers/pessoas.js
+++ src/controllers/pessoas.js
@@ -55,9 +55,15 @@ exports.postUsuarios = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
+    res.status(201).send({
+        restfull: "clientes",
+        method: "post",
+        req: req
+    });
+
     
-        mysql_connection.query(
-            'INSERT INTO pessoas(nome, aniversario, email, senha, e_admin, celular) VALUES(?, ?, ?, ?, "N", ?)'
+        /* mysql_connection.query(
+            'INSERT INTO pessoas(nome, aniversario, email, senha, e_admin, celular) VALUES(?, ?, ?, ?, "N", ?)',
             [req.body.nome, req.body.aniversario, req.body.email, req.body.senha, req.body.celular],
             (error, result) => {
                 if (error) {
@@ -71,7 +77,7 @@ exports.postUsuarios = (req, res) => {
                     method: "post",
                     req: result
                 });
-            });
+            }); */
      
 };
 

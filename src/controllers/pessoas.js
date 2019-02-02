324d17a9d8802ab92193ba49f5bb14d10203eb7f
Rogerio <rogerio_pavan@hotmail.com>
2019-02-02 18:50:37 -0200

    commit

--- src/controllers/pessoas.js
+++ src/controllers/pessoas.js
@@ -55,20 +55,21 @@ exports.postUsuarios = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    mysql_connection.query(
-        'INSERT INTO pessoas(nome, aniversario, senha, celular) VALUES(?, ?, ?, ?)',
-       [req.body.nome, req.body.aniversario,  req.body.senha, req.body.celular], 
-
-        (error, result) => {
-            if (error) {
-                console.log(error);
-                res.status(500).json({ "error_code": error.code });
-                return;
-            }
-
-            res.status(201).json(result);
-        }); 
-     
+    bcrypt.hash( req.body.senha, saltRounds, (err, hash) =>{
+        mysql_connection.query(
+            'INSERT INTO pessoas(nome, aniversario, senha, celular) VALUES(?, ?, ?, ?)',
+           [req.body.nome, req.body.aniversario, hash, req.body.celular], 
+    
+            (error, result) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({ "error_code": error.code });
+                    return;
+                }
+    
+                res.status(201).json(result);
+            }); 
+    });  
 };
 
 exports.put = (req, res) => {

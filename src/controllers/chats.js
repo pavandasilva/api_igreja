af938c12c343aedf45791638dd3b38dc9d4d9f6c
Rogerio <rogerio_pavan@hotmail.com>
2019-02-03 15:51:39 -0200

    commit

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -9,6 +9,12 @@ exports.get = ((req, res) => {
 });
 
 exports.post = (req, res) => {
+   /*  Verifica se o usuário dono da mensagem é o mesmo usuario dono do token */
+    if(req.body.pessoa_id_rem === req.pessoa_id){
+        res.status(401).json({error: "Você não tem autorização para enviar essa mensagem"});
+        return;
+    }
+
     mysql_connection.query(
         'INSERT INTO chats(pessoa_id_dest, pessoa_id_rem, texto) VALUES(?, ?, ?)',
         [req.body.pessoa_id_dest, req.body.pessoa_id_rem, req.body.texto],

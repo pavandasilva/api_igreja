433415ddc0800af75dd07f56069aeaf8cb20e651
Rogerio <rogerio_pavan@hotmail.com>
2019-02-02 22:02:18 -0200

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const cors = require('cors');

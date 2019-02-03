1e51fe9b62c338c2612f9d650d28ae0a4eb67b46
Rogerio <rogerio_pavan@hotmail.com>
2019-02-03 15:12:10 -0200

    commit

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/chat');
+const controller = require('../controllers/chats');
 /* const authMiddleware = require('../middlewares/autenticacao'); */
 
 /* router.use(authMiddleware); */

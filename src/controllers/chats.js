9fa0e0c652985c5e4a3c0da06e11eac599140c7e
Rogerio <rogerio_pavan@hotmail.com>
2019-02-03 16:10:51 -0200

    commit

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -10,7 +10,7 @@ exports.get = ((req, res) => {
 
 exports.post = (req, res) => {
 
-    res.status(401).send(req.body.texto);
+    res.status(200).send(req.body.texto);
     return;
    /*  Verifica se o usuário dono da mensagem é o mesmo usuario dono do token */
     if(req.body.pessoa_id_rem != req.pessoa_id){

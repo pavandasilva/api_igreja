f9a43bd8f55c8a786f87ad21bbc19184f01352ad
Rogerio <rogerio_pavan@hotmail.com>
2019-02-05 22:27:38 -0200

    commit

--- bin/server.js
+++ bin/server.js
@@ -9,6 +9,13 @@ app.set('port', port);
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
 server.listen(port);
 server.on('listening', onListening);
 

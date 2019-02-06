0320c4635583afc88add4fd2d9d9d49af895c7d7
Rogerio <rogerio_pavan@hotmail.com>
2019-02-05 22:21:10 -0200

    commit

--- src/controllers/pessoas.js
+++ src/controllers/pessoas.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const autenticacao = require('../../config/autenticacao');
 const saltRounds = 10;
-const io = require('../bin/server');
 
 /* GET usuários comuns */
 exports.getUsuarios = (req, res) => {

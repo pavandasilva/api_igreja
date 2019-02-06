72121519c319a7719ac96ca85eb1aa5299a446b0
Rogerio <rogerio_pavan@hotmail.com>
2019-02-06 01:29:18 -0200

    commit

--- bin/server.js
+++ bin/server.js
@@ -12,9 +12,7 @@ const io = require('socket.io')(server);
 io.on('connection', function (socket) {
     console.log(socket.id);
 
-    socket.on('vinc_socket_id'), function () {
-        console.log('teste');
-    };
+    socket.on('reply', function(){ /* */ });
 });
 
 

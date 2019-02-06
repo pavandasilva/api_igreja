e4e7f39b04d737595256ece772a93ecfb4e682d8
Rogerio <rogerio_pavan@hotmail.com>
2019-02-05 22:15:14 -0200

    commit

--- bin/server.js
+++ bin/server.js
@@ -1,26 +1,13 @@
 const app = require('../src/app');
 const debug = require('debug')('igreja:server');
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
 app.set('port', port);
 
 const server = http.createServer(app);
 const io = require('socket.io')(server);
-
-var socket = require('socket.io'), http_io = require('http'),
-  server_io = http_io.createServer(), socket = socket.listen(server_io);
-socket.on('connection', function(connection) {
-   console.log('User Connected');
-   
-   connection.on('message', function(msg){
-     socket.emit('message', msg);
-   });
-});
-
-server.listen(4000, function(){
-    console.log('Server started');
-});
 
 server.listen(port);
 server.on('listening', onListening);

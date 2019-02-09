c79d764f8a4d85c5b178452d8c7302fe72ed9be4
Rogerio <rogerio_pavan@hotmail.com>
2019-02-09 15:15:31 -0200

    commit

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -57,7 +57,7 @@ exports.post = (req, res) => {
                     }); */
 
                     require('../../bin/server').then((socket)=>{
-                        socket.emit('chat', JSON.parse(rows));
+                        socket.emit('chat', 'teste' );
                     });
                     
                     res.status(201).json(rows);

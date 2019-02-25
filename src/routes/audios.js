d2779d7c030eda1f586461c637ffd0e2ff5a7a5c
Rogerio <rogerio_pavan@hotmail.com>
2019-02-25 00:52:46 -0300

    commit

--- src/routes/audios.js
+++ src/routes/audios.js
@@ -4,5 +4,5 @@ const controller = require('../controllers/audios');
 
 router.get('/', controller.get);
 router.post('/', controller.post);
-router.post_like('/like', controller.post_like);
+router.post('/like', controller.post_like);
 module.exports = router;

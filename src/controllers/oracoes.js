416b2aebf44048f7edea9ca1ab463223e7f36fb5
Rogerio <rogerio_pavan@hotmail.com>
2019-02-25 02:09:00 -0300

    commit

--- src/controllers/oracoes.js
+++ src/controllers/oracoes.js
@@ -10,12 +10,12 @@ exports.get = ((req, res) => {
             'pessoas.pessoa_id, ' +
             'pessoas.nome, ' +
             'pessoas.foto, ' +
-            'COUNT(oracoes_pessoas.oracao.pessoa_id) as qtde_pessoas'
+            'COUNT(oracoes_pessoas.oracao.pessoa_id) as qtde_pessoas ' +
         'FROM oracoes ' +
         'INNER JOIN pessoas ' +
         'ON oracoes.pessoa_id = pessoas.pessoa_id ' +
         'LEFT JOIN oracoes_pessoas ' +
-        'ON oracoes.pessoa_id = oracoes_pessoas.pessoa_id'
+        'ON oracoes.pessoa_id = oracoes_pessoas.pessoa_id '
         'GROUP BY oracoes.oracao_id, oracoes.texto, oracoes.dt_cadastro, pessoas.pessoa_id, pessoas.nome, pessoas.foto';
    
     mysql_connection.query(sql, (error, rows, fields) => {

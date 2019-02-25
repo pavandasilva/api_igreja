91767b33d119d82a924443794d8bbcc0bc3308be
Rogerio <rogerio_pavan@hotmail.com>
2019-02-25 02:48:54 -0300

    commit

--- src/routes/oracoes.js
+++ src/routes/oracoes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/oracoes');
+const authMiddleware = require('../middlewares/autenticacao');
+
+router.use(authMiddleware);
 
 router.get('/', controller.get);
 router.post('/orar', controller.post_orar);

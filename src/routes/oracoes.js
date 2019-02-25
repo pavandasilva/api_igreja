9a64fcd4e2e8a9b07352853b1ace8e98f3fa46f6
Rogerio <rogerio_pavan@hotmail.com>
2019-02-25 02:53:45 -0300

    commit

--- src/routes/oracoes.js
+++ src/routes/oracoes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/oracoes');
 const authMiddleware = require('../middlewares/autenticacao');
 
-router.use(authMiddleware);
-
 router.get('/', controller.get);
+
+// endpoint que passam pela autenticação
+router.use(authMiddleware);
 router.post('/orar', controller.post_orar);
 module.exports = router;

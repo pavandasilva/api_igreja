f0203673dd34777bc81a8dfac905b33225750edc
Rogerio <rogerio_pavan@hotmail.com>
2019-02-25 01:12:27 -0300

    commit

--- src/controllers/audios.js
+++ src/controllers/audios.js
@@ -10,10 +10,17 @@ exports.get = ((req, res) => {
         'audios.url, ' +
         'audios.dt_cadastro, ' +
         'categorias_audi.categoria_audi_id, ' +
-        'categorias_audi.nome as categoria ' +
+        'categorias_audi.nome as categoria, ' +
+        'count(audios_like.audio_id) as qtde_likes, ' +
+        'count(audios_like.audio_id) as qtde_compartilhamentos ' +
         'FROM audios ' +
         'INNER JOIN categorias_audi ' +
-        'ON audios.categoria_audi_id = categorias_audi.categoria_audi_id';
+        'ON audios.categoria_audi_id = categorias_audi.categoria_audi_id ' +
+        ' LEFT JOIN audios_like ' +
+        ' ON audios.audio_id = audios_like.audio_id ' +
+        ' LEFT JOIN audios_compartilhamentos ' +
+        ' ON audios.audio_id = audios_compartilhamentos.audio_id ' +
+        ' GROUP BY audios.audio_id, audios.titulo, audios.autor, audios.url, audios.dt_cadastro, categorias_audi.categoria_audi_id, categoria ';
 
     mysql_connection.query(sql, (error, rows, fields) => {
         res.header("Access-Control-Allow-Origin", "*");
